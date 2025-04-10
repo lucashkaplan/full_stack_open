d803226913d70d650c59dd195be2ad4dfd818e91
Lucas Kaplan <lhkaplan53@gmail.com>
2025-04-10 14:03:37 -0400

    Added route to get info about phonebook. Tested and works

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,25 +1,52 @@
-const express = require('express')
+const express = require('express');
 // create express app and store in app var
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 const fs = require('fs');
+
+// get info for all people in phonebook
+const personsJSON = JSON.parse(fs.readFileSync('./persons.json', 'utf8'));
 
 // route to access all contacts
 app.get('/api/persons', (request, response) => {
-    const personsJSON = JSON.parse(fs.readFileSync('./persons.json', 'utf8'));
-
     if(personsJSON){
-        response.json(personsJSON)
+        response.json(personsJSON);
     }
     else{
         response.status(404).json({ 
             error: 'content missing' 
-        })
+        });
     }
 })
 
-const PORT = 3001
+app.get('/info', (request, response) => {
+    // get number of people in phonebook
+    const numContacts = Array.isArray(personsJSON)
+        ? personsJSON.length
+        : 0;
+    
+    // Get the current time
+    const requestTime = new Date().toLocaleString();
+
+    // html response
+    const htmlResponse = `
+        <html>
+            <div>
+                Phonebook has info for ${numContacts} people.
+            </div>
+            <div>
+                ${requestTime}
+            </div>
+        </html>
+    `;
+    
+    // set content type to HTML and send response
+    response.setHeader('Content-Type', 'text/html');
+    response.send(htmlResponse);
+})
+
+const PORT = 3001;
 // create server (listens for conections on port 3001)
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
-})
+});
